53b5e8dd16694cb9db634b73da36ec633501d74a
Przemysław Lenart <przemek.lenart@polidea.com>
2018-06-15 12:53:42 +0200

    Initial project setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// @flow
+
+import { AppRegistry } from "react-native";
+
+import App from "./js/App";
+
+AppRegistry.registerComponent("ReduxSagaArchitecture", () => App);
---
+++ js/App.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React, { Component } from 'react'
+import { Text, SafeAreaView } from 'react-native'
+
+type Props = {}
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>ReactNativeArchitecture</Text>
+      </SafeAreaView>
+    )
+  }
+}
